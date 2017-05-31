7b10ab82446e2b6e7672ce4a91583154bea2d46c
Kim-Seonghyeon <shkim93@snu.ac.kr>
2017-05-31 15:08:14 +0900

    Update model_utils.py

--- model_utils.py
+++ model_utils.py
@@ -140,7 +140,7 @@ def EqualSpaceFrames_ver2(model_input, num_frames, num_samples):
     `model_input`: A tensor of size batch_size x num_samples x feature_size
   """
   batch_size = tf.shape(model_input)[0]
-  frame_index = tf.cast(tf.tile(tf.expand_dims(tf.range(num_samples), 0), [batch_size, 1]), tf.float32) * 2.
+  frame_index = tf.cast(tf.tile(tf.expand_dims(tf.range(num_samples), 0), [batch_size, 1]), tf.float32) * 3.
   index = tf.cast(frame_index, tf.int32)
   return tf.gather_nd(model_input, index)
 
