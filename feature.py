d44f4ca8ee1686fe6043da26f7e578ce29a8a661
Kim-Seonghyeon <shkim93@snu.ac.kr>
2017-06-05 17:10:39 +0900

    Update feature.py

--- feature.py
+++ feature.py
@@ -50,7 +50,7 @@ if __name__ == '__main__':
 
 
   # Other flags.
-  flags.DEFINE_integer("num_readers", 8,
+  flags.DEFINE_integer("num_readers", 1,
                        "How many threads to use for reading input files.")
   flags.DEFINE_integer("top_k", 20,
                        "How many predictions to output per video.")
